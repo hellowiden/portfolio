98db731efe8c99b1a830bf314962405abcf0e40f
Marcus Widen <hellowiden@gmail.com>
2025-03-04 13:49:32 +0100

    added custom font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Space_Grotesk } from 'next/font/google';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const spaceGrotesk = Space_Grotesk({
   subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  weight: ['300', '400', '500', '700'],
+  style: ['normal'],
 });
 
 export const metadata: Metadata = {
@@ -24,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${spaceGrotesk.className} antialiased`}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <h1 className="text-6xl ">hello</h1>;
+export default function Typography() {
+  return (
+    <div className="space-y-4 p-6">
+      <h1 className="text-6xl font-bold tracking-tight">Hello Georgius!</h1>
+      <h2 className="text-5xl font-semibold tracking-tight">
+        Elegant Simplicity
+      </h2>
+      <h3 className="text-4xl font-medium tracking-normal">
+        Aesthetic Precision
+      </h3>
+      <h4 className="text-3xl font-medium tracking-normal">
+        Design with Purpose
+      </h4>
+      <h5 className="text-2xl font-medium tracking-wide">Subtle and Sharp</h5>
+      <h6 className="text-xl font-medium tracking-wide">
+        Harmony in Typography
+      </h6>
+      <p className="text-lg font-light leading-relaxed">
+        The essence of typography lies in balance, contrast, and rhythm, forming
+        a visual hierarchy that guides the eye effortlessly.
+      </p>
+    </div>
+  );
 }
