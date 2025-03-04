8e74832ecc30c1ca5807f9bfabf43097e30bb8d6
Marcus Widen <hellowiden@gmail.com>
2025-03-04 15:26:00 +0100

    added header

---
+++ src/app/components/Header/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="bg-amber-100 p-2">
+      <h1 className="text-2xl font-bold tracking-tight text-amber-600">
+        MW-Portfolio
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
