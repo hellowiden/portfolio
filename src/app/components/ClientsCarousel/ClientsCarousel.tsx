ce18cc0fc37fb6ecaebc6233969795a0c7043542
Marcus Widen <hellowiden@gmail.com>
2025-03-06 20:15:20 +0100

    added ClientsCarousel.tsx

--- src/app/components/ClientsCarousel/ClientsCarousel.tsx
+++ src/app/components/ClientsCarousel/ClientsCarousel.tsx
@@ -30,16 +30,17 @@ function ClientsCarousel() {
   ];
 
   return (
-    <section id="clients" className="p-6">
-      <h2 className="text-2xl font-bold">My Clients</h2>
-      <div className="grid grid-cols-2 gap-2">
+    <section id="clients" className=" grid gap-2 p-6 bg-zinc-900 text-zinc-100">
+      <h2 className="text-2xl font-bold text-zinc-300 text-center">
+        My Clients
+      </h2>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
         {clients.map((client, index) => (
-          <span
-            key={index}
-            className=" bg-blue-100 text-blue-800 text-sm font-medium px-3 py-2 rounded-full text-center"
-          >
-            {client}
-          </span>
+          <div key={index} className="flex justify-center items-center">
+            <span className="bg-zinc-800 text-zinc-300 text-sm font-medium px-4 py-2 rounded-full text-center">
+              {client}
+            </span>
+          </div>
         ))}
       </div>
     </section>
