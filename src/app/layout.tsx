132ce59209ab253873d9dbb4f5755494459dfaf3
Marcus Widen <hellowiden@gmail.com>
2025-03-06 08:09:11 +0100

    tailwind fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 // src/app/layout.tsx
-// src/app/layout.tsx
 
 import type { Metadata } from 'next';
 import { Space_Grotesk } from 'next/font/google';
@@ -28,14 +27,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${spaceGrotesk.className} grid min-h-screen grid-rows-[auto_1fr_auto] bg-zinc-50`}
+        className={`${spaceGrotesk.className} !scroll-smooth grid min-h-screen grid-rows-[auto_1fr_auto] bg-zinc-50`}
       >
         <AuthProvider>
           <div className="grid grid-rows-[auto_1fr_auto] min-h-screen">
             <Header />
-            <main className="container mx-auto flex-grow px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20 2xl:px-24 py-16 border-x border-light dark:bg-black dark:border-dark backdrop-blur-md">
-              {children}
-            </main>
+            <main className="backdrop-blur-md">{children}</main>
             <Footer />
           </div>
         </AuthProvider>
