2891adefc32e965023ac25d5b34d3e40f0cbb923
Marcus Widen <hellowiden@gmail.com>
2025-03-06 13:53:19 +0100

    added IntroductionSection.tsx

--- src/app/components/IntroductionSection/IntroductionSection.tsx
+++ src/app/components/IntroductionSection/IntroductionSection.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
+import Image from 'next/image';
 
 function IntroductionSection() {
   return (
     <section
       id="home"
-      className="w-full   gap-6 rounded-md bg-zinc-200 p-6 transition-all"
+      className="w-full gap-6 rounded-md bg-zinc-200 p-6 transition-all"
     >
       <div
         className="w-40 h-40 rounded-full overflow-hidden border-2 border-zinc-600"
         aria-label="Profile Picture"
       >
-        <div className="w-full h-full bg-zinc-700 ">Image</div>
+        <Image
+          src="/MW.png"
+          alt="Marcus Widén"
+          width={160}
+          height={160}
+          className="w-full h-full object-cover"
+        />
       </div>
       <div className="text-4xl font-bold tracking-wide">Marcus Widén</div>
-      <div className=" text-md tracking-wide font-normal leading-relaxed text-zinc-400 ">
+      <div className="text-md tracking-wide font-normal leading-relaxed text-zinc-400">
         Hi, I’m Marcus Widén. I’m passionate about helping businesses thrive
         through strategic planning, user-focused design, and creative
         problem-solving. Let’s work together to turn your ideas into impactful
         solutions.
       </div>
-      <div className=" w-full grid-cols-2 gap-4 ">
-        <button className="rounded-md border px-6 py-3 text-sm font-light transition border-zinc-600 bg-black text-white ">
+      <div className="w-full grid grid-cols-2 gap-4">
+        <button className="rounded-md border px-6 py-3 text-sm font-light transition border-zinc-600 bg-black text-white">
           Contact
         </button>
         <button className="rounded-md border px-6 py-3 text-sm font-light transition border-zinc-600 bg-black text-white">
