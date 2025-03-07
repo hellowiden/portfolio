8a8a1e9e888448126b2356e354167fcfd1a0e7f9
Marcus Widen <hellowiden@gmail.com>
2025-03-07 16:52:45 +0100

    style footer

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -5,22 +5,26 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className="w-full border-t p-6 grid  gap-10 border-light dark:border-dark bg-white/70 dark:bg-black/70 transition">
-      <div className="max-w-screen-xl mx-auto py-10 p-6 sm:px-8 grid grid-cols-1 md:grid-cols-2 gap-10 text-start">
+    <footer className="w-full border-t border-zinc-200 dark:border-zinc-800 bg-zinc-50/70 dark:bg-zinc-900/70 transition">
+      <div className="max-w-screen-xl p-6 mx-auto py-10 px-6 sm:px-8 grid grid-cols-1 md:grid-cols-2 gap-10 text-start">
         <div>
-          <h4 className="text-xl font-semibold mb-4">Portfolio Techstack</h4>
-          <p className="text-sm text-black dark:text-white">
+          <h4 className="text-xl font-semibold text-zinc-900 dark:text-zinc-50 mb-4">
+            Portfolio Techstack
+          </h4>
+          <p className="text-sm text-zinc-700 dark:text-zinc-300">
             Built with React, Next.js, TypeScript, Tailwind CSS, and Framer
             Motion, hosted on Vercel.
           </p>
         </div>
         <div>
-          <h4 className="text-xl font-semibold mb-4">Docs</h4>
+          <h4 className="text-xl font-semibold text-zinc-900 dark:text-zinc-50 mb-4">
+            Docs
+          </h4>
           <ul className="space-y-2">
             <li>
               <Link
                 href="/legal"
-                className="text-sm text-green-700 hover:text-green-500 dark:text-green-300 dark:hover:text-green-400 transition"
+                className="text-sm text-green-600 hover:text-green-500 dark:text-green-400 dark:hover:text-green-300 transition"
               >
                 Legal
               </Link>
@@ -28,7 +32,7 @@ export default function Footer() {
           </ul>
         </div>
       </div>
-      <div className="border-t border-light dark:border-dark mt-10 py-4 text-center text-sm text-black dark:text-white">
+      <div className="border-t py-2 border-zinc-200 dark:border-zinc-800 mt-10 py-4 text-center text-sm text-zinc-700 dark:text-zinc-300">
         &copy; {new Date().getFullYear()} Marcus Widén — Personal Portfolio. All
         rights reserved.
       </div>
