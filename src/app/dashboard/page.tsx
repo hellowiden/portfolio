905ff59cdc680b274a8585e17925088b619ac044
Marcus Widen <hellowiden@gmail.com>
2025-03-07 15:12:15 +0100

    added darkmode

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -122,7 +122,7 @@ export default function Dashboard() {
                 Email
               </th>
               <th className="px-4 py-2 text-left border border-zinc-300">
-                Roles
+                Role
               </th>
               <th className="px-4 py-2 text-left border border-zinc-300">
                 Actions
