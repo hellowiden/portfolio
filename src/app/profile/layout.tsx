b3ddb1c3b423401ef67b93c8566e9bc6b7c7b7a8
Marcus Widen <hellowiden@gmail.com>
2025-03-08 09:01:19 +0100

    style

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -5,11 +5,11 @@ export default function ProfileLayout({ children }: { children: ReactNode }) {
     <div className="h-full grid place-items-center p-6">
       <div
         role="main"
-        className="max-w-4xl w-full bg-white dark:bg-zinc-800 p-6 rounded border border-zinc-300 dark:border-zinc-700 grid gap-4"
+        className="max-w-4xl w-full bg-white dark:bg-zinc-800 p-6 rounded-lg border border-zinc-300 dark:border-zinc-700 grid gap-4"
       >
-        <p className="text-center text-zinc-900 dark:text-zinc-100 font-semibold">
+        <h1 className="text-center text-zinc-900 dark:text-zinc-100 font-semibold">
           Your Profile
-        </p>
+        </h1>
         {children}
       </div>
     </div>
