a517b9b29e6afec234f82eb816f1003301b62f57
Marcus Widen <hellowiden@gmail.com>
2025-03-09 18:26:59 +0100

    introduction section

--- src/app/components/IntroductionSection/IntroductionSection.tsx
+++ src/app/components/IntroductionSection/IntroductionSection.tsx
@@ -1,24 +1,15 @@
 'use client';
 
 import Image from 'next/image';
-import { useCallback } from 'react';
 
 export default function IntroductionSection() {
-  const handleContactClick = useCallback(() => {
-    console.log('Contact button clicked');
-  }, []);
-
-  const handleDownloadClick = useCallback(() => {
-    console.log('Download Resume button clicked');
-  }, []);
-
   return (
     <section
       id="home"
       className="w-full h-[500px] grid place-items-end bg-cover bg-center overflow-hidden"
       style={{ backgroundImage: "url('/shake.jpg')" }}
     >
-      <div className="w-full grid gap-6 bg-black bg-opacity-70 backdrop-blur-md p-6">
+      <div className="w-full  grid gap-6 bg-black bg-opacity-70 backdrop-blur-md p-6">
         <div className="grid gap-4 text-white w-full max-w-3xl">
           <div className="grid grid-cols-[auto_1fr] items-center gap-4">
             <Image
@@ -40,19 +31,17 @@ export default function IntroductionSection() {
             planning, user-focused design, and creative problem-solving. Let’s
             work together to turn your ideas into impactful solutions.
           </p>
-          <div className="grid grid-cols-2 gap-4">
-            <button
-              onClick={handleContactClick}
-              className="w-full transition hover:text-white"
-            >
+          <div className="grid grid-cols-[min-content,auto] items-start gap-4">
+            <a href="#contact" className="transition hover:text-white">
               Contact
-            </button>
-            <button
-              onClick={handleDownloadClick}
-              className="w-full transition hover:text-white"
+            </a>
+            <a
+              href="/resume.pdf"
+              download
+              className=" transition hover:text-white"
             >
               Download Resume
-            </button>
+            </a>
           </div>
         </div>
       </div>
