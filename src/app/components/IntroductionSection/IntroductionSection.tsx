826c72a9eb3689b351eb6de2a6d528548508bfae
Marcus Widen <hellowiden@gmail.com>
2025-03-09 19:04:39 +0100

    introduction section

--- src/app/components/IntroductionSection/IntroductionSection.tsx
+++ src/app/components/IntroductionSection/IntroductionSection.tsx
@@ -9,37 +9,42 @@ export default function IntroductionSection() {
       className="w-full h-[500px] grid place-items-end bg-cover bg-center overflow-hidden"
       style={{ backgroundImage: "url('/shake.jpg')" }}
     >
-      <div className="w-full grid grid-cols-[1fr,min-content] gap-4 bg-black bg-opacity-70 backdrop-blur-md p-6">
-        <div className="grid gap-4 text-white w-full">
-          <div className="grid grid-cols-[auto_1fr] items-center gap-4">
-            <Image
-              src="/MW.png"
-              alt="Marcus Widén"
-              width={100}
-              height={100}
-              className="rounded-full border"
-            />
-            <div className="grid gap-1">
-              <h1 className="text-3xl font-bold">Marcus Widén</h1>
-              <span className="text-sm bg-gray-800/70 px-3 py-1 rounded-md opacity-80">
-                Brand Developer
-              </span>
-              <span className="text-sm bg-gray-800/70 px-3 py-1 rounded-md opacity-80">
-                Full stack developer
-              </span>
-            </div>
-          </div>
-          <p className="opacity-80">
+      <div className="w-full grid grid-cols-2 grid-rows-2 gap-2 bg-zinc-800 bg-opacity-75 backdrop-blur-sm p-6 text-white">
+        {/* Row 1: Spanning Both Columns */}
+        <div className="col-span-2">
+          <h1 className="text-3xl">Brand and Fullstack Developer</h1>
+          <p className="opacity-80 tracking-wide">
             I’m passionate about helping businesses thrive through strategic
             planning, user-focused design, and creative problem-solving. Let’s
             work together to turn your ideas into impactful solutions.
           </p>
         </div>
+
+        {/* Row 2, Column 1 */}
+        <div className="grid grid-cols-[auto_1fr] items-center gap-3">
+          <Image
+            src="/MW.png"
+            alt="Marcus Widén"
+            width={40}
+            height={40}
+            className="rounded-full border"
+          />
+          <h1 className="text-xl">Marcus Widén</h1>
+        </div>
+
+        {/* Row 2, Column 2 */}
         <div className="grid grid-cols-[min-content,auto] items-start justify-end gap-4">
-          <a href="#contact" className="transition text-white">
+          <a
+            href="#contact"
+            className="transition text-sm opacity-55 hover:opacity-100 text-white"
+          >
             Contact
           </a>
-          <a href="/resume.pdf" download className="transition text-white">
+          <a
+            href="/resume.pdf"
+            download
+            className="transition text-sm opacity-55 hover:opacity-100 text-white"
+          >
             Download Resume
           </a>
         </div>
