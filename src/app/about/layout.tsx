0f562cd33969da00abaddba7bcf0462a54409319
Marcus Widen <hellowiden@gmail.com>
2025-03-10 13:03:21 +0100

    styling

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react';
 
 export default function AboutLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="h-full flex flex-col items-center justify-center bg-zinc-100 dark:bg-zinc-900">
+    <div className="h-full flex flex-col items-center border-x dark:border-light justify-center bg-zinc-100 dark:bg-zinc-900">
       <main className="w-full p-6">{children}</main>
     </div>
   );
