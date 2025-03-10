05d87221072002f6d54ffe1e2467b849e9fa0402
Marcus Widen <hellowiden@gmail.com>
2025-03-10 19:27:05 +0100

    styling

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react';
 
 export default function AboutLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="h-full flex flex-col items-center border-x dark:border-light justify-center bg-zinc-100 dark:bg-zinc-800">
+    <div className="h-full flex flex-col items-center border-x dark:border-light justify-center bg-white dark:bg-zinc-900">
       {/* Header */}
       <header className="w-full bg-zinc-200 dark:bg-zinc-900 text-zinc-900 dark:text-white p-4">
         <div className="container mx-auto flex justify-between items-center">
