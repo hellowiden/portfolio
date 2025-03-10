e770bf9852780ccfacbde3036ad96d44c7278577
Marcus Widen <hellowiden@gmail.com>
2025-03-10 10:23:01 +0100

    legal page now supports darkmode

--- src/app/components/IntroductionSection/IntroductionSection.tsx
+++ src/app/components/IntroductionSection/IntroductionSection.tsx
@@ -23,21 +23,7 @@ export default function IntroductionSection() {
 
       {/* Content Overlay */}
       <div className="relative w-full grid grid-cols-[1fr_auto] grid-rows-[min-content, min-content] gap-4 bg-white/75 dark:bg-zinc-800/75 backdrop-blur-md p-6 text-zinc-900 dark:text-white">
-        {/* Row 1: Full Width */}
-        <div className="col-span-2">
-          <h1 className="text-3xl font-semibold">
-            Elevating Brands, Engineering Impact
-          </h1>
-          <p className="opacity-80 tracking-wide">
-            In a world where attention is power, your brand must be more than
-            visible—it must be undeniable. I specialize in strategic branding,
-            full-stack development, and market positioning that commands trust
-            and influence. My expertise ensures your business stands out,
-            resonates deeply, and drives measurable success.
-          </p>
-        </div>
-
-        {/* Row 2, Column 1 */}
+        {/* Row 1, Column 1 */}
         <div className="flex items-center gap-3">
           <Image
             src="/MW.png"
@@ -50,6 +36,7 @@ export default function IntroductionSection() {
           <h1 className="text-xl font-medium">Marcus Widén</h1>
         </div>
 
+        {/* Row 1, Column 2 */}
         <div className="flex items-center gap-4 text-sm">
           <Link
             href="#contact"
@@ -64,6 +51,20 @@ export default function IntroductionSection() {
           >
             Download Resume
           </Link>
+        </div>
+
+        {/* Row 2, Full Width */}
+        <div className="col-span-2">
+          <h1 className="text-3xl font-semibold">
+            Elevating Brands, Engineering Impact
+          </h1>
+          <p className="opacity-80 tracking-wide">
+            In a world where attention is power, your brand must be more than
+            visible—it must be undeniable. I specialize in strategic branding,
+            full-stack development, and market positioning that commands trust
+            and influence. My expertise ensures your business stands out,
+            resonates deeply, and drives measurable success.
+          </p>
         </div>
       </div>
     </section>
