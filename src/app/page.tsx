7de65b60658a3048895ce31667e5ef1b59e185db
Marcus Widen <hellowiden@gmail.com>
2025-03-11 21:27:43 +0100

    adding contact component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import ClientsCarousel from './components/ClientsCarousel/ClientsCarousel';
 import IntroductionSection from './components/IntroductionSection/IntroductionSection';
 import ProjectOpener from './components/ProjectOpener/ProjectOpener';
 import ExperienceOpener from './components/ExperienceOpener/ExperienceOpener';
+import ContactForm from './components/ContactForm/ContactForm';
 
 export default function Home() {
   const { status } = useSession();
@@ -30,6 +31,7 @@ export default function Home() {
           <ProjectOpener />
           <ExperienceOpener />
         </div>
+        <ContactForm />
       </main>
     </>
   );
