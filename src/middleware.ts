4d347b257cc0dfec88fc23700c2be7866291152b
Marcus Widen <hellowiden@gmail.com>
2025-03-12 14:28:49 +0100

    adding contact component

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,17 +4,12 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
 export async function middleware(req: NextRequest) {
-  console.log('Middleware Running: ', req.nextUrl.pathname); // Debugging log
-
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-
-  const protectedRoutes = ['/', '/dashboard/:path*', '/about'];
 
   if (
     !token &&
-    protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route))
+    config.matcher.some((route) => req.nextUrl.pathname.startsWith(route))
   ) {
-    console.log('Redirecting to login:', req.nextUrl.pathname);
     return NextResponse.redirect(new URL('/login', req.url));
   }
 
