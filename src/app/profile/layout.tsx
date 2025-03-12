4a87bda7d0dca92fd777b9393d1d53ff717cfc36
Marcus Widen <hellowiden@gmail.com>
2025-03-12 21:11:35 +0100

    style

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -6,7 +6,7 @@ import { ReactNode } from 'react';
 
 export default function ProfileLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen border-x dark:border-light bg-zinc-100 dark:bg-zinc-800">
+    <div className="flex flex-col border-x dark:border-light bg-zinc-100 dark:bg-zinc-800">
       {/* Header */}
       <header className="bg-zinc-200 dark:bg-zinc-900 text-zinc-900 dark:text-white p-4">
         <div className="container mx-auto flex justify-between items-center">
