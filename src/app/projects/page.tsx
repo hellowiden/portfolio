50a763ce6eb9dcd9ba072c7cbcf2c39439ba2ff2
Marcus Widen <hellowiden@gmail.com>
2025-03-12 15:11:40 +0100

    opt

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,53 +3,16 @@
 'use client';
 
 import Link from 'next/link';
-
-const projects = [
-  {
-    id: 'project-1',
-    name: 'Green Furniture Concept',
-    tags: ['Brand Strategy', 'Sustainability', 'Design'],
-  },
-  {
-    id: 'project-2',
-    name: 'E-Commerce Storefront',
-    tags: ['React', 'Firebase', 'Tailwind'],
-  },
-  {
-    id: 'project-3',
-    name: 'Personal Portfolio',
-    tags: ['Next.js', 'Tailwind', 'Vercel'],
-  },
-  {
-    id: 'project-4',
-    name: 'Weather App',
-    tags: ['Vue.js', 'API', 'Geolocation'],
-  },
-  {
-    id: 'project-5',
-    name: 'Task Manager',
-    tags: ['Angular', 'Node.js', 'MongoDB'],
-  },
-  {
-    id: 'project-6',
-    name: 'Airam',
-    tags: ['Lighting Solutions', 'Brand Identity', 'Marketing'],
-  },
-  {
-    id: 'project-7',
-    name: 'Droga5',
-    tags: ['Advertising', 'Creative Strategy', 'Brand Positioning'],
-  },
-];
+import { projects } from '@/data/projects';
 
 export default function ProjectsPage() {
   return (
-    <div className=" ">
-      <div className="grid gap-6 bg-zinc-100 dark:bg-zinc-900 p-4 border border-zinc-300 dark:border-zinc-700 rounded-xl">
+    <div className="grid gap-4 text-zinc-900 dark:text-zinc-100">
+      <div className="grid gap-4 bg-zinc-100 dark:bg-zinc-900 p-4 border border-zinc-300 dark:border-zinc-700 rounded-xl">
         {projects.map((project) => (
           <div
             key={project.id}
-            className="border-b last:border-none border-zinc-300 dark:border-zinc-700"
+            className="border-b last:border-none border-zinc-300 dark:border-zinc-700 pb-2"
           >
             <Link
               href={`/projects/${project.id}`}
