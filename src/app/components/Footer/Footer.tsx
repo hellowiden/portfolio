f92601cd5df1d1229994f8f5639a216877dd91e2
Marcus Widen <hellowiden@gmail.com>
2025-03-13 20:22:52 +0100

    clean-up comments

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -3,18 +3,13 @@
 'use client';
 
 import Link from 'next/link';
-import { useState, useEffect } from 'react';
 
 export default function Footer() {
-  const [currentYear, setCurrentYear] = useState<number | null>(null);
-
-  useEffect(() => {
-    setCurrentYear(new Date().getFullYear());
-  }, []);
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer className="w-full z-20 border-t border-zinc-200 dark:border-zinc-800 bg-white/80 dark:bg-black/70 transition">
-      <div className="w-full px-6 sm:px-8 py-10 grid gap-6 grid-cols-1 md:grid-cols-[1fr_auto] items-end">
+      <div className="w-full px-6 sm:px-8 py-10 grid gap-6 md:grid-cols-[1fr_auto]">
         <div>
           <h4 className="text-xl font-semibold text-zinc-900 dark:text-zinc-50 mb-4">
             Portfolio Techstack
@@ -31,13 +26,13 @@ export default function Footer() {
           <Link
             href="/legal"
             prefetch={false}
-            className="text-sm text-black/60  dark:text-green-400 hover:text-green-500 dark:hover:text-green-300 transition"
+            className="text-sm font-medium text-black/60 dark:text-green-400 hover:text-green-500 dark:hover:text-green-300 transition"
           >
             Legal
           </Link>
         </div>
       </div>
-      <div className="grid gap-4 border-t border-zinc-200 dark:border-zinc-800 p-4 text-sm text-zinc-700 dark:text-zinc-300 w-full sm:px-8 grid-cols-1 md:grid-cols-[1fr_auto] items-end">
+      <div className="grid gap-4 border-t border-zinc-200 dark:border-zinc-800 p-4 text-sm text-zinc-700 dark:text-zinc-300 sm:px-8 md:grid-cols-[1fr_auto]">
         <p>&copy; {currentYear} Marcus Widén — Personal Portfolio.</p>
         <p>All rights reserved.</p>
       </div>
