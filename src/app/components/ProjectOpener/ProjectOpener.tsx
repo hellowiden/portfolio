b696172cbeef60b52beb2034278a3f6dc641f595
Marcus Widen <hellowiden@gmail.com>
2025-03-13 20:20:43 +0100

    clean-up comments

--- src/app/components/ProjectOpener/ProjectOpener.tsx
+++ src/app/components/ProjectOpener/ProjectOpener.tsx
@@ -9,43 +9,40 @@ import React from 'react';
 interface ProjectOpenerProps {
   title?: string;
   description?: string;
-  link?: string;
 }
 
-function ProjectOpener({
+export default function ProjectOpener({
   title = 'My Projects',
-  description = 'Take a look at my most recent projects and let that become proof of what you can expect from me',
+  description = 'Take a look at my most recent projects and let that become proof of what you can expect from me.',
 }: ProjectOpenerProps) {
   return (
-    <section className="w-full border dark:border-light rounded-xl container mx-auto bg-cover bg-center overflow-hidden grid grid-cols-1 grid-rows-1 relative">
+    <section className="border dark:border-light rounded-xl bg-cover bg-center overflow-hidden relative grid">
       <Image
         src="/projectsopener.jpg"
         alt={title}
-        fill={true}
+        fill
         sizes="(max-width: 768px) 100vw, 50vw"
-        style={{ objectFit: 'cover' }}
-        className="absolute top-0 left-0 w-full h-full object-cover"
+        className="absolute inset-0 object-cover"
         priority
       />
 
-      <div className="relative w-full grid grid-cols-2 grid-rows-[auto_auto] gap-4 bg-zinc-50/75 dark:bg-zinc-800/75 backdrop-blur-sm p-6 text-zinc-900 dark:text-white">
-        <div className="flex items-center gap-3 col-span-1">
+      <div className="relative bg-zinc-50/75 dark:bg-zinc-800/75 backdrop-blur-sm p-6 text-zinc-900 dark:text-white grid gap-4">
+        <div className="flex justify-between items-center">
           <h1 className="text-xl font-medium text-zinc-900 dark:text-zinc-100">
             {title}
           </h1>
-        </div>
-        <div className="flex justify-end items-center col-span-1">
           <Link
             href="/projects"
-            className="flex items-center gap-2 text-sm dark:border-dark  text-black/60 rounded hover:text-black dark:text-white/50  dark:hover:text-white transition hover:text-opacity-80 dark:hover:text-opacity-80"
+            className="text-sm font-medium text-black/60 dark:text-white/50 hover:text-black dark:hover:text-white transition"
           >
             View Projects
           </Link>
         </div>
-        <div className="col-span-2">
-          <div className="text-2xl font-bold text-zinc-900 dark:text-zinc-100">
+
+        <div>
+          <h2 className="text-2xl font-bold text-zinc-900 dark:text-zinc-100">
             Case Studies
-          </div>
+          </h2>
           <p className="opacity-80 tracking-wide max-w-[900px] text-zinc-700 dark:text-zinc-300">
             {description}
           </p>
@@ -54,5 +51,3 @@ function ProjectOpener({
     </section>
   );
 }
-
-export default ProjectOpener;
