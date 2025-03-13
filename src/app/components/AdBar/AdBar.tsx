6fd5a18c7161d5aef0f97526f0739fde453ecec0
Marcus Widen <hellowiden@gmail.com>
2025-03-13 20:08:07 +0100

    clean-up comments

--- src/app/components/AdBar/AdBar.tsx
+++ src/app/components/AdBar/AdBar.tsx
@@ -5,32 +5,30 @@
 import { useEffect, useState, useCallback, useRef } from 'react';
 import { adMessages } from '../../../data/adMessages';
 
-const messages = adMessages;
-
 export default function AdBar() {
   const [index, setIndex] = useState(0);
   const messageIntervalRef = useRef<NodeJS.Timeout | null>(null);
 
   const rotateMessage = useCallback(() => {
-    setIndex((prevIndex) => (prevIndex + 1) % messages.length);
+    setIndex((prevIndex) => (prevIndex + 1) % adMessages.length);
   }, []);
 
   useEffect(() => {
-    if (messageIntervalRef.current) clearInterval(messageIntervalRef.current);
-
     messageIntervalRef.current = setInterval(rotateMessage, 60000);
 
-    return () => {
-      if (messageIntervalRef.current) clearInterval(messageIntervalRef.current);
-    };
+    return () => clearInterval(messageIntervalRef.current!);
   }, [rotateMessage]);
 
+  if (adMessages.length === 0) return null;
+
   return (
-    <div className="grid place-items-center  text-white p-6 text-center w-full gap-4">
+    <div className="grid place-items-center text-white p-6 text-center w-full gap-4">
       <h2 className="text-3xl font-bold" aria-live="polite">
-        &ldquo;{messages[index].heading}&rdquo;
+        &ldquo;{adMessages[index]?.heading || 'Default Heading'}&rdquo;
       </h2>
-      <p className="text-lg">{messages[index].subtext}</p>
+      <p className="text-lg">
+        {adMessages[index]?.subtext || 'Default subtext message'}
+      </p>
       <div className="w-full max-w-md h-1 bg-zinc-700 rounded-full overflow-hidden relative">
         <div className="w-full h-full bg-white progress-bar" />
       </div>
@@ -38,7 +36,6 @@ export default function AdBar() {
         .progress-bar {
           animation: progressAnimation 60s linear infinite;
         }
-
         @keyframes progressAnimation {
           from {
             width: 0%;
