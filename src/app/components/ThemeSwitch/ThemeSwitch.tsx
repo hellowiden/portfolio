e8105f19087e50fae542e41d1c586530e391b409
Marcus Widen <hellowiden@gmail.com>
2025-03-13 16:19:44 +0100

    hydration issues hopefully fixed

--- src/app/components/ThemeSwitch/ThemeSwitch.tsx
+++ src/app/components/ThemeSwitch/ThemeSwitch.tsx
@@ -2,38 +2,37 @@
 
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { useTheme } from '@/context/theme-context';
 import { FiMoon, FiSun } from 'react-icons/fi';
 
 export default function ThemeSwitch() {
   const { theme, toggleTheme } = useTheme();
-  const [isHovered, setIsHovered] = useState(false);
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   const isLight = theme === 'light';
 
   return (
     <button
       onClick={toggleTheme}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
       aria-label={`Switch to ${isLight ? 'dark' : 'light'} mode`}
       className="grid grid-cols-[auto_1fr] items-center p-2 text-sm border rounded transition bg-white dark:bg-black text-black dark:text-white hover:bg-zinc-800 hover:text-white dark:hover:bg-zinc-600 dark:border-zinc-600 sm:gap-2"
     >
       <motion.div
-        key={isHovered ? 'hover' : theme}
+        key={theme}
         initial={{ rotate: -90, opacity: 0 }}
         animate={{ rotate: 0, opacity: 1 }}
         exit={{ rotate: 90, opacity: 0 }}
         transition={{ duration: 0.3 }}
       >
-        {isHovered ? (
-          isLight ? (
-            <FiMoon className="text-lg" />
-          ) : (
-            <FiSun className="text-lg" />
-          )
-        ) : isLight ? (
+        {isLight ? (
           <FiSun className="text-lg" />
         ) : (
           <FiMoon className="text-lg" />
