0a15dd460412de88590becb73aa7ad02dc11cdeb
Marcus Widen <hellowiden@gmail.com>
2025-03-13 07:40:24 +0100

    style

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -34,9 +34,12 @@ export default function About() {
             className="grid gap-4"
           >
             <h1 className="text-3xl font-medium text-zinc-900 dark:text-zinc-50">
-              Why me? In short, because your brand deserves to lead.
+              Why me?
             </h1>
             <p className="text-md text-zinc-700 dark:text-zinc-300 leading-relaxed">
+              <strong>
+                In short, because informed strategy fuels enduring success.
+              </strong>{' '}
               I don’t just build brands—I shape legacies. With a decade of
               experience leading 500+ projects, I’ve proven that a brand’s power
               lies in its ability to command attention, foster loyalty, and
@@ -53,8 +56,8 @@ export default function About() {
               <br />
               My approach is both analytical and visionary, turning complexity
               into clarity and brand potential into results. Executives trust me
-              to simplify challenges and architect brands that don’t just adapt
-              but dictate market shifts.
+              to decode market shifts, simplify challenges, and architect brands
+              that resonate on a deeper level.
               <br />
               <br />
               Beyond business, I draw from history, philosophy, and human
