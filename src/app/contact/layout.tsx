746ba0b165f3678a2c0e83f6e0100488d894529a
Marcus Widen <hellowiden@gmail.com>
2025-03-13 15:12:56 +0100

    styles

--- src/app/contact/layout.tsx
+++ src/app/contact/layout.tsx
@@ -6,7 +6,7 @@ import { ReactNode } from 'react';
 
 export default function ContactLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="h-full flex flex-col items-center border-x dark:border-light justify-center bg-zinc-100 dark:bg-zinc-900">
+    <div className="h-full flex flex-col items-center container mx-auto border-x dark:border-light backdrop-blur-md bg-zinc-100/80 dark:bg-zinc-900/80">
       {/* Header */}
       <header className="w-full bg-zinc-200 dark:bg-zinc-900 text-zinc-900 dark:text-white p-4">
         <div className="container mx-auto flex justify-between items-center">
