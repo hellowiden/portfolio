9b51f6d16af3d0e50dcb74ea4d37b19c615a1bfd
Marcus Widen <hellowiden@gmail.com>
2025-03-13 10:32:39 +0100

    opt

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,25 +7,36 @@ import { projects } from '@/data/projects';
 
 export default function ProjectsPage() {
   return (
-    <div className="grid gap-4 text-zinc-900 dark:text-zinc-100">
-      <div className="grid gap-4 bg-zinc-100 dark:bg-zinc-900 p-4 border border-zinc-300 dark:border-zinc-700 rounded-xl">
-        {projects.map((project) => (
-          <div
-            key={project.id}
-            className="border-b last:border-none border-zinc-300 dark:border-zinc-700 pb-2"
-          >
-            <Link
-              href={`/projects/${project.id}`}
-              className="text-green-600 dark:text-green-400 hover:underline"
-            >
-              {project.name}
-            </Link>
-            <div className="text-xs text-gray-600 dark:text-gray-400">
-              {project.tags.join(' • ')}
-            </div>
+    <>
+      <section className="grid gap-4">
+        <p className="italic">
+          <strong>Note:</strong> The majority of my past projects are protected
+          by strict NDAs, preventing any disclosure. However, I’ve learned my
+          lesson—moving forward, projects will be less restricted, and I plan to
+          showcase them here. If you’d like to discuss how I can bring value to
+          your brand, let’s connect.
+        </p>
+        <div className="grid gap-4 text-zinc-900 dark:text-zinc-100">
+          <div className="grid gap-4 bg-zinc-100 dark:bg-zinc-900 p-4 border border-zinc-300 dark:border-zinc-700 rounded-xl">
+            {projects.map((project) => (
+              <div
+                key={project.id}
+                className="border-b last:border-none border-zinc-300 dark:border-zinc-700 pb-2"
+              >
+                <Link
+                  href={`/projects/${project.id}`}
+                  className="text-green-600 dark:text-green-400 hover:underline"
+                >
+                  {project.name}
+                </Link>
+                <div className="text-xs text-gray-600 dark:text-gray-400">
+                  {project.tags.join(' • ')}
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
+      </section>
+    </>
   );
 }
