a49fd0be5acf0a59d1d971229e300a3f7cd85f91
Marcus Widen <hellowiden@gmail.com>
2025-03-14 09:42:41 +0100

    styles

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,13 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
-const PROTECTED_ROUTES = ['/dashboard', '/about', '/experiences']; // Define high-level protected routes
-
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  const path = req.nextUrl.pathname;
 
-  // Check if the request matches any protected route dynamically
-  const requiresAuth = PROTECTED_ROUTES.some((route) => path.startsWith(route));
-
-  if (!token && requiresAuth) {
+  if (
+    !token &&
+    config.matcher.some((route) => req.nextUrl.pathname.startsWith(route))
+  ) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
 
@@ -20,5 +17,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next|api|static|public|favicon.ico).*)'], // Matches all except Next.js internals
+  matcher: ['/', '/dashboard/:path*', '/about', '/experiences/:path*'],
 };
