fe5e79ec028f2026033ec0fc3ec266b2954e3914
Marcus Widen <hellowiden@gmail.com>
2025-03-17 13:35:15 +0100

    Deployed 3

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -15,10 +15,10 @@ export default function Layout({ children }: LayoutProps) {
   const { data: session, status } = useSession();
   const router = useRouter();
   const pathname = usePathname();
-
-  const isAdmin = useMemo(() => {
-    return session?.user?.roles.includes('admin');
-  }, [session]);
+  const isAdmin = useMemo(
+    () => session?.user?.roles.includes('admin'),
+    [session]
+  );
 
   useEffect(() => {
     if (
@@ -29,36 +29,25 @@ export default function Layout({ children }: LayoutProps) {
     }
   }, [status, isAdmin, router]);
 
-  if (status === 'loading') return <p>Loading...</p>;
-  if (!isAdmin) return null;
+  if (status === 'loading' || !isAdmin) return null;
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-[auto_1fr] h-full bg-zinc-100 dark:bg-zinc-900 text-zinc-900 dark:text-white gap-4">
-      {/* SIDEBAR NAVIGATION */}
-      <aside className="flex flex-col gap-4 w-64 bg-zinc-200 dark:bg-zinc-800 border-r dark:border-zinc-700 p-4">
+    <div className="grid md:grid-cols-[auto_1fr] h-full bg-zinc-100 dark:bg-zinc-900 text-zinc-900 dark:text-white">
+      <aside className="w-64 bg-zinc-200 dark:bg-zinc-800 p-4 border-r dark:border-zinc-700">
         <h2 className="text-xl font-bold">Admin Dashboard</h2>
         <nav className="grid gap-2">
-          <NavItem href="/dashboard" label="Dashboard" pathname={pathname} />
-          <NavItem href="/dashboard/users" label="Users" pathname={pathname} />
-          <NavItem
-            href="/dashboard/projects"
-            label="Projects"
-            pathname={pathname}
-          />
-          <NavItem
-            href="/dashboard/messages"
-            label="Messages"
-            pathname={pathname}
-          />
-          <NavItem
-            href="/dashboard/experiences"
-            label="Experiences"
-            pathname={pathname}
-          />
+          {['Dashboard', 'Users', 'Projects', 'Messages', 'Experiences'].map(
+            (label) => (
+              <NavItem
+                key={label}
+                href={`/dashboard/${label.toLowerCase()}`}
+                label={label}
+                pathname={pathname}
+              />
+            )
+          )}
         </nav>
       </aside>
-
-      {/* MAIN CONTENT */}
       <main className="p-4">{children}</main>
     </div>
   );
