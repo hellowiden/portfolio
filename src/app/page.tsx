02e42614f59f218e65a52077fef78947e9ad7a80
Marcus Widen <hellowiden@gmail.com>
2025-03-18 13:58:41 +0100

    Deployed 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,13 @@ import ContactOpener from '@/app/components/ContactOpener/ContactOpener';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[1fr , min-content , 1fr] gap-6 w-full h-full p-6 items-start container mx-auto border-x dark:border-light backdrop-blur-md bg-zinc-100/80 dark:bg-zinc-900/80">
+    <div className="container mx-auto h-full w-full p-6 border-x dark:border-light backdrop-blur-md bg-zinc-100/80 dark:bg-zinc-900/80">
       <IntroductionSection />
       <ClientsCarousel />
-      <div className="grid grid-cols-1 sm:grid-cols-2 grid-rows-3 sm:grid-rows-2 gap-6">
-        <div className="col-span-1 row-span-1">
-          <ProjectOpener />
-        </div>
-        <div className="col-span-1 row-span-1">
-          <ExperienceOpener />
-        </div>
-        <div className=" col-span-1 sm:col-span-2 row-span-1">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+        <ProjectOpener />
+        <ExperienceOpener />
+        <div className="sm:col-span-2">
           <ContactOpener />
         </div>
       </div>
