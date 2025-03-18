39e04a0a1a53e45d6ceb37fe7c796242bb0eeae9
Marcus Widen <hellowiden@gmail.com>
2025-03-18 19:27:55 +0100

    test for simplify

--- src/app/components/ContactOpener/ContactOpener.tsx
+++ src/app/components/ContactOpener/ContactOpener.tsx
@@ -14,7 +14,7 @@ export default function ContactOpener() {
 
   return (
     <section
-      className="border dark:border-light rounded-xl bg-cover bg-center overflow-hidden relative grid h-[250px] "
+      className="border dark:border-light rounded-xl bg-cover bg-center overflow-hidden relative grid h-[300px] md:h-[400px] lg:h-[500px]"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
