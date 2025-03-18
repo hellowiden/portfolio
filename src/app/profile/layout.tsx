b5dd5f1e67a45d1cbb7b72c511cdeb25e7b709dd
Marcus Widen <hellowiden@gmail.com>
2025-03-18 15:44:34 +0100

    Deployed 4

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -6,7 +6,7 @@ import { ReactNode } from 'react';
 
 export default function ProfileLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col container mx-auto border-x dark:border-light backdrop-blur-md bg-zinc-100/80 dark:bg-zinc-900/80">
+    <div className="flex flex-col h-full container mx-auto border-x dark:border-light backdrop-blur-md bg-zinc-100/80 dark:bg-zinc-900/80">
       {/* Header */}
       <header className="bg-zinc-200 dark:bg-zinc-900 text-zinc-900 dark:text-white p-4">
         <div className="container mx-auto flex justify-between items-center">
