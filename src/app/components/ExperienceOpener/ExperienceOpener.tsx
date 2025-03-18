d777a9ff0e7dc24c94edc5a028b6209d9971a5b0
Marcus Widen <hellowiden@gmail.com>
2025-03-18 19:37:17 +0100

    test for simplify

--- src/app/components/ExperienceOpener/ExperienceOpener.tsx
+++ src/app/components/ExperienceOpener/ExperienceOpener.tsx
@@ -14,7 +14,7 @@ export default function ExperienceOpener() {
 
   return (
     <section
-      className="border dark:border-light rounded-xl bg-cover bg-center overflow-hidden relative grid h-[300px] md:h-[400px] lg:h-[500px]"
+      className="border dark:border-light rounded-xl bg-cover bg-center overflow-hidden relative grid h-[300px] md:h-[300px] lg:h-[300px]"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
