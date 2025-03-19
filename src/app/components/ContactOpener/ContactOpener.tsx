70f2e27b19695fecd1aa658aa335126a1d6e4c3c
Marcus Widen <hellowiden@gmail.com>
2025-03-19 07:28:45 +0100

    test for simplify 2

--- src/app/components/ContactOpener/ContactOpener.tsx
+++ src/app/components/ContactOpener/ContactOpener.tsx
@@ -14,7 +14,7 @@ export default function ContactOpener() {
 
   return (
     <section
-      className="relative bg-zinc-50/75 dark:bg-zinc-800/75 backdrop-blur-sm border dark:border-light rounded-xl bg-cover bg-center overflow-hidden grid h-[300px] md:h-[300px] lg:h-[300px] place-items-center"
+      className="bg-zinc-50/75 dark:bg-zinc-800/75 backdrop-blur-sm border dark:border-light rounded-xl bg-cover bg-center overflow-hidden relative grid h-[300px] md:h-[300px] lg:h-[300px] place-items-center"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
@@ -31,20 +31,20 @@ export default function ContactOpener() {
         {isHovered ? (
           <>
             <div className="grid grid-cols-[1fr_auto] items-start w-full max-w-[900px]">
-              <h2 className="text-xl font-medium text-black dark:text-white">
+              <h1 className="text-xl font-medium text-black dark:text-black">
                 Get in Touch
-              </h2>
+              </h1>
               <button
                 onClick={() => router.push('/contact')}
                 aria-label="Contact Me"
-                className="flex items-center gap-2 p-2 text-sm border rounded transition backdrop-blur-md bg-white dark:bg-black text-black dark:text-white hover:bg-zinc-800 hover:text-white dark:hover:bg-zinc-600 dark:border-zinc-600"
+                className="grid grid-cols-[auto_1fr] items-center p-2 text-sm border rounded transition backdrop-blur-md bg-white dark:bg-black text-black dark:text-white hover:bg-zinc-800 hover:text-white dark:hover:bg-zinc-600 dark:border-zinc-600 sm:gap-2"
               >
                 <motion.div
+                  key="contact"
                   initial={{ rotate: -90, opacity: 0 }}
                   animate={{ rotate: 0, opacity: 1 }}
                   exit={{ rotate: 90, opacity: 0 }}
                   transition={{ duration: 0.3 }}
-                  aria-hidden="true"
                 >
                   <FiMail className="text-lg" />
                 </motion.div>
@@ -52,7 +52,7 @@ export default function ContactOpener() {
               </button>
             </div>
             <div>
-              <h2 className="text-2xl font-bold text-black dark:text-white">
+              <h2 className="text-2xl font-bold text-black dark:text-black">
                 Let&apos;s Connect
               </h2>
               <p className="opacity-80 tracking-wide max-w-[900px] text-zinc-700 dark:text-zinc-300">
