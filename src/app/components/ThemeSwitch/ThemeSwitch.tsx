c8dcf447a669276995b8c863643683b8ca5eca1f
Marcus Widen <hellowiden@gmail.com>
2025-03-19 14:45:58 +0100

    testing new theme

--- src/app/components/ThemeSwitch/ThemeSwitch.tsx
+++ src/app/components/ThemeSwitch/ThemeSwitch.tsx
@@ -18,7 +18,7 @@ export default function ThemeSwitch() {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       aria-label="Toggle theme"
-      className="grid grid-cols-[auto_1fr] items-center p-2 text-sm sm:gap-2 border rounded transition text-black bg-zinc-100 hover:bg-zinc-200 border-zinc-300 dark:text-white dark:bg-zinc-700 dark:hover:bg-zinc-800 dark:border-zinc-900"
+      className="grid grid-cols-[auto_1fr] items-center p-2 text-sm sm:gap-2 border rounded transition text-black bg-zinc-100 hover:bg-zinc-200 border-zinc-300 dark:text-white dark:bg-zinc-700 dark:hover:bg-zinc-800 dark:border-zinc-600"
     >
       <motion.div
         key={isHovered ? 'hover' : theme}
