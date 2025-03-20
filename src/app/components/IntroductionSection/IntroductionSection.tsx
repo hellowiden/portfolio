f7b541dcab2025bda043a36aa00b49afb631fe4c
Marcus Widen <hellowiden@gmail.com>
2025-03-20 14:06:46 +0100

    destructing header

--- src/app/components/IntroductionSection/IntroductionSection.tsx
+++ src/app/components/IntroductionSection/IntroductionSection.tsx
@@ -6,7 +6,7 @@ function IntroductionSection() {
   return (
     <section className="grid items-center h-[500px] rounded-xl p-6 text-center bg-zinc-100 dark:bg-zinc-800 border border-zinc-300 dark:border-zinc-700">
       <div className="grid gap-4">
-        <h1 className="text-4xl font-bold text-zinc-900 dark:text-zinc-100">
+        <h1 className="text-8xl font-bold text-zinc-900 dark:text-zinc-100">
           Wisdom Creates Wealth.
         </h1>
         <p className="text-md text-zinc-800 dark:text-zinc-300">
