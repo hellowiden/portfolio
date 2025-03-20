289601864ab14617cf5cf2c6c61a062c1ea2f0c0
Marcus Widen <hellowiden@gmail.com>
2025-03-20 13:11:09 +0100

    destructing header

--- src/app/components/IntroductionSection/IntroductionSection.tsx
+++ src/app/components/IntroductionSection/IntroductionSection.tsx
@@ -5,9 +5,9 @@ import React from 'react';
 function IntroductionSection() {
   return (
     <>
-      <section className="grid items-center rounded-xl p-6 bg-black text-zinc-100">
-        <h1>Wisdom Creates Wealth.</h1>
-        <p>
+      <section className="grid items-center rounded-xl p-6 bg-black">
+        <h1 className="text-2xl text-zinc-100">Wisdom Creates Wealth.</h1>
+        <p className="text-md text-zinc-200">
           True success is not about chasing profits, but about mastering the
           principles that make wealth inevitable.
         </p>
