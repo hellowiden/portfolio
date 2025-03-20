1ee25b49596929a696d43992ecd665caf7c7eb78
Marcus Widen <hellowiden@gmail.com>
2025-03-20 15:27:44 +0100

    destructing header

--- src/app/components/IntroductionSection/IntroductionSection.tsx
+++ src/app/components/IntroductionSection/IntroductionSection.tsx
@@ -2,38 +2,46 @@
 
 'use client';
 
-import Image from 'next/image';
+import { useState, useEffect, useRef } from 'react';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
 import { motion } from 'framer-motion';
+import Image from 'next/image';
 import { FiFileText } from 'react-icons/fi';
+import { brandingMessages } from './../../../data/brandingMessages';
 
-function IntroductionSection() {
+const messages = brandingMessages;
+
+export default function IntroductionSection() {
   const router = useRouter();
+  const [messageIndex, setMessageIndex] = useState(0);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
   const [isHovered, setIsHovered] = useState(false);
-  const messages = [{ heading: 'Welcome', subtext: 'Discover more about us.' }];
-  const messageIndex = 0;
+
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      setMessageIndex((prevIndex) => (prevIndex + 1) % brandingMessages.length);
+    }, 15000);
+
+    return () => clearInterval(intervalRef.current!);
+  }, []);
 
   return (
-    <section className="grid grid-rows-2 w-full h-[500px] rounded-xl text-center overflow-hidden bg-zinc-100 dark:bg-zinc-800 border border-zinc-300 dark:border-zinc-700">
-      {/* Video in the first row */}
-      <div className="w-full h-full">
-        <video
-          autoPlay
-          loop
-          muted
-          playsInline
-          className="w-full h-full object-cover"
-          aria-label="Promotional Video"
-        >
-          <source src="/ads.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
+    <section
+      id="home"
+      className="w-full h-[500px] border-2 dark:border-light rounded-xl container mx-auto bg-cover bg-center overflow-hidden flex items-end relative gap-2"
+    >
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute top-0 left-0 w-full h-full object-cover"
+      >
+        <source src="/ads.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
-      {/* Content in the second row */}
-      <div className="grid grid-cols-2 grid-rows-2 gap-4 bg-white/85 dark:bg-zinc-800/85 backdrop-blur-md p-6 text-zinc-900 dark:text-white w-full">
-        {/* Row 1, Column 1 */}
+      <div className="relative w-full grid grid-cols-[1fr_auto] grid-rows-[auto_auto] gap-4 bg-white/85 dark:bg-zinc-800/85 backdrop-blur-md p-6 text-zinc-900 dark:text-white">
         <div className="grid grid-cols-[auto_1fr] items-center gap-3">
           <Image
             src="/MW.png"
@@ -46,7 +54,6 @@ function IntroductionSection() {
           <h1 className="text-xl font-medium">Marcus Widén</h1>
         </div>
 
-        {/* Row 1, Column 2 */}
         <button
           className="grid grid-cols-[auto_1fr] items-center p-2 text-sm sm:gap-2 border rounded transition text-black bg-zinc-100 hover:bg-zinc-200 border-zinc-300 dark:text-white dark:bg-zinc-700 dark:hover:bg-zinc-800 dark:border-zinc-600"
           onClick={() => router.push('/about')}
@@ -65,18 +72,30 @@ function IntroductionSection() {
           <span className="ml-2">About me</span>
         </button>
 
-        {/* Row 2, Column 1 & 2 (Spanning Both Columns) */}
-        <div className="col-span-2">
-          <h2 className="text-3xl font-bold">
+        <div className="grid grid-rows-[auto_auto] gap-2 col-span-2 md:col-span-1">
+          <motion.h2
+            key={messageIndex}
+            className="text-3xl font-bold"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.8, ease: 'easeInOut' }}
+          >
             {messages[messageIndex].heading}
-          </h2>
-          <p className="opacity-80 tracking-wide max-w-[900px]">
+          </motion.h2>
+
+          <motion.p
+            key={`subtext-${messageIndex}`}
+            className="opacity-80 tracking-wide max-w-[900px]"
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -10 }}
+            transition={{ duration: 0.8, ease: 'easeInOut' }}
+          >
             {messages[messageIndex].subtext}
-          </p>
+          </motion.p>
         </div>
       </div>
     </section>
   );
 }
-
-export default IntroductionSection;
