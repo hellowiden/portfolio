4697b03fecf3ad21e6dd14385a9ef466932184b9
Marcus Widen <hellowiden@gmail.com>
2025-03-20 13:42:49 +0100

    destructing header

--- src/app/components/IntroductionSection/IntroductionSection.tsx
+++ src/app/components/IntroductionSection/IntroductionSection.tsx
@@ -6,13 +6,15 @@ function IntroductionSection() {
   return (
     <>
       <section className="grid items-center h-[500] rounded-xl p-6 bg-black text-center">
-        <h1 className="text-4xl font-bold text-zinc-100">
-          Wisdom Creates Wealth.
-        </h1>
-        <p className="text-md text-zinc-200">
-          True success is not about chasing profits, but about mastering the
-          principles that make wealth inevitable.
-        </p>
+        <div className="grid gap-4">
+          <h1 className="text-4xl font-bold text-zinc-100">
+            Wisdom Creates Wealth.
+          </h1>
+          <p className="text-md text-zinc-200">
+            True success is not about chasing profits, but about mastering the
+            principles that make wealth inevitable.
+          </p>
+        </div>
       </section>
     </>
   );
