d3fda156b9d45175322e2f621c46cdb55bbec56f
Marcus Widen <hellowiden@gmail.com>
2025-03-20 12:50:25 +0100

    destructing header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,19 +9,13 @@ import ContactOpener from '@/app/components/ContactOpener/ContactOpener';
 
 export default function Home() {
   return (
-    <div className="grid gap-2 h-full container mx-auto p-6 border-x dark:border-light bg-zinc-100/80 dark:bg-zinc-900/80">
+    <div className="grid gap-6 h-full container mx-auto p-6 border-x dark:border-light">
       <IntroductionSection />
       <ClientsCarousel />
-
-      {/* Dynamic Bento Grid Layout */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 auto-rows-fr gap-2">
-        <div className="lg:col-span-2">
-          <ProjectOpener />
-        </div>
-        <div className="lg:col-span-2 lg:row-span-2">
-          <ExperienceOpener />
-        </div>
-        <div className="lg:col-span-2">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+        <ProjectOpener />
+        <ExperienceOpener />
+        <div className="sm:col-span-2">
           <ContactOpener />
         </div>
       </div>
