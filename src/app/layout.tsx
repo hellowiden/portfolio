e9b15c5ffaf42e665dc493f5e51120915e1b355d
Marcus Widen <hellowiden@gmail.com>
2025-03-21 09:30:35 +0100

    try backgronddots

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={spaceGrotesk.variable}>
-      <body className="grid min-h-screen grid-rows-[auto_1fr_auto] bg-zinc-200/70 dark:bg-zinc-900/70 text-zinc-800 dark:text-zinc-200">
+      <body className="grid min-h-screen grid-rows-[auto_1fr_auto] text-zinc-800 dark:text-zinc-200">
         <ThemeContextProvider>
           <AuthProvider>
             <BackgroundDots />
