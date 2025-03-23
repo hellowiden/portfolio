15d048798e76f2610071986b01532aa994f563fc
Marcus Widen <hellowiden@gmail.com>
2025-03-23 14:29:25 +0100

    font size

--- src/app/components/ClientsCarousel/ClientsCarousel.tsx
+++ src/app/components/ClientsCarousel/ClientsCarousel.tsx
@@ -1,8 +1,8 @@
-//src/app/components/ClientsCarousel/ClientsCarousel.tsx
+// src/app/components/ClientsCarousel/ClientsCarousel.tsx
 
 import Marquee from 'react-fast-marquee';
 import { motion } from 'framer-motion';
-import { clientsData } from '../../../data/clients';
+import { clientsData } from '@/data/clients';
 
 export default function ClientsCarousel() {
   return (
@@ -13,7 +13,7 @@ export default function ClientsCarousel() {
       <Marquee
         gradient={false}
         speed={50}
-        className="grid grid-flow-col auto-cols-max gap-2"
+        className="grid auto-flow-col auto-cols-max gap-2"
       >
         {clientsData.map(({ name, url }) => (
           <motion.a
@@ -21,14 +21,14 @@ export default function ClientsCarousel() {
             href={url}
             target="_blank"
             rel="noopener noreferrer"
-            className="font-medium relative gap-2 group whitespace-nowrap bg-green-100 text-green-700 dark:bg-green-800 dark:text-green-200 text-sm px-2 py-1 rounded-full"
+            className="font-medium relative gap-2 group whitespace-nowrap bg-green-100 text-green-700 dark:bg-green-800 dark:text-green-200 text-sm px-2 py-1 rounded-full focus:outline-none focus:ring-2 focus:ring-green-500"
             aria-label={`Client: ${name}`}
             whileHover={{ scale: 1.05 }}
             transition={{ type: 'spring', stiffness: 300, damping: 20 }}
           >
             {name}
-            <motion.div
-              className="absolute bottom-0 left-0 h-[2px]  w-full scale-x-0 group-hover:scale-x-100 transition-transform duration-300 "
+            <div
+              className="absolute bottom-0 left-0 h-[2px] w-full scale-x-0 group-hover:scale-x-100 transition-transform duration-300"
               aria-hidden="true"
             />
           </motion.a>
