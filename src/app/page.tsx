038f9cef45ce3239295ad476c5d09b18f0aa8e29
Marcus Widen <hellowiden@gmail.com>
2025-03-23 15:09:44 +0100

    font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,10 @@ export default function Home() {
     <div className="grid grid-rows-[500px_auto_auto_auto] gap-6 h-full container mx-auto p-6 border-x dark:border-light bg-zinc-100 dark:bg-zinc-800 text-zinc-900 dark:text-zinc-100 border-zinc-300 dark:border-zinc-700">
       <IntroductionSection />
       <ClientsCarousel />
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+      <div className="grid sm:grid-cols-2 grid-rows-2 gap-6 auto-rows-fr">
         <ProjectOpener />
         <ExperienceOpener />
-        <div className="sm:col-span-2">
+        <div className="sm:col-span-2  sm:row-span-2">
           <ContactOpener />
         </div>
       </div>
