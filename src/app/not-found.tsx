bfac342e2da5031d7657197e71942e3247f46d66
Marcus Widen <hellowiden@gmail.com>
2025-03-23 21:09:18 +0100

    custom page not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,17 +6,17 @@ export default function NotFound() {
     <div className="grid place-items-center container mx-auto bg-zinc-100 dark:bg-zinc-800 border-x border-zinc-300 dark:border-zinc-700 min-h-full p-6">
       <div className="grid gap-6 text-center">
         <h1 className="text-4xl font-bold text-zinc-900 dark:text-zinc-50">
-          404 – You’ve Hit a Dead End
+          404 – Woopsie Daisies! You’ve Hit a Dead End
         </h1>
         <p className="text-lg text-zinc-600 dark:text-zinc-400">
-          Either the page vanished into the void, or you’ve discovered a secret
-          level. Either way, it’s not here.
+          Either the file was deleted, or someone forgot to push to{' '}
+          <code>main</code>. Happens to the best of us. 😘
         </p>
         <Link
           href="/"
           className="text-base font-medium text-green-600 hover:text-green-500 dark:text-green-400 dark:hover:text-green-300 transition"
         >
-          Go back home
+          Return to safety
         </Link>
       </div>
     </div>
