d75ce673e31cbccefc50f0e5f2c576a138a7ce99
Marcus Widen <hellowiden@gmail.com>
2025-03-23 11:25:03 +0100

    font size

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -23,7 +23,7 @@ export default function About() {
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
-          className="grid gap-6 text-center md:text-left"
+          className="grid gap-6 text-left w-full"
         >
           <motion.div
             initial={{ scale: 0.8 }}
@@ -34,9 +34,11 @@ export default function About() {
             <h1 className="text-4xl font-medium text-zinc-900 dark:text-zinc-50">
               Why Me?
             </h1>
+
             <p className="text-xl text-zinc-900 dark:text-zinc-100 font-semibold">
               Because clarity is rare, and strategy is everything.
             </p>
+
             <p className="text-base text-zinc-700 dark:text-zinc-300 leading-relaxed">
               In a world where most chase noise, I focus on what lasts. I don’t
               build brands to go viral—I build them to endure. With over a
@@ -69,9 +71,10 @@ export default function About() {
               just about branding—it’s about seeing the game for what it is and
               playing it to win.
             </p>
+
             <Link
               href="/contact"
-              className="text-lg font-medium grid grid-cols-[auto_1fr] items-center p-2 sm:gap-2 border rounded transition border-green text-green-700 hover:bg-green-100 dark:border-green dark:text-green-200 dark:hover:bg-green-800"
+              className="text-lg font-medium w-fit grid grid-cols-[auto_1fr] items-center p-2 sm:gap-2 border rounded transition border-green text-green-700 hover:bg-green-100 dark:border-green dark:text-green-200 dark:hover:bg-green-800"
             >
               If you seek strategy, let’s talk.
             </Link>
