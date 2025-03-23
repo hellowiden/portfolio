9a6de6dd0d14c07158d3db63b6acc1a0a312ec64
Marcus Widen <hellowiden@gmail.com>
2025-03-23 15:12:45 +0100

    font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ import ContactOpener from '@/app/components/ContactOpener/ContactOpener';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[500px_auto_auto_auto] gap-6 h-full container mx-auto p-6 border-x dark:border-light bg-zinc-100 dark:bg-zinc-800 text-zinc-900 dark:text-zinc-100 border-zinc-300 dark:border-zinc-700">
+    <div className="grid grid-rows-[500px_auto_auto_auto] gap-6 min-h-screen container mx-auto p-6 border-x dark:border-light bg-zinc-100 dark:bg-zinc-800 text-zinc-900 dark:text-zinc-100 border-zinc-300 dark:border-zinc-700">
       <IntroductionSection />
       <ClientsCarousel />
-      <div className="grid sm:grid-cols-2 grid-rows-2 gap-6 auto-rows-fr">
+      <div className="grid sm:grid-cols-2 auto-rows-fr gap-6">
         <ProjectOpener />
         <ExperienceOpener />
-        <div className="sm:col-span-2  sm:row-span-2">
+        <div className="sm:col-span-2 sm:row-span-2 h-full">
           <ContactOpener />
         </div>
       </div>
