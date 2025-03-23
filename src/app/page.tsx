561843cdf65c63ccf23903199e733354ddae6075
Marcus Widen <hellowiden@gmail.com>
2025-03-23 16:15:08 +0100

    openers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <div className="grid gap-6 min-h-screen container mx-auto p-6 border-x dark:border-light bg-zinc-100 dark:bg-zinc-800 text-zinc-900 dark:text-zinc-100 border-zinc-300 dark:border-zinc-700">
       <IntroductionSection />
       <ClientsCarousel />
-      <div className="grid grid-cols-2 grid-rows-2 gap-6">
+      <div className="grid grid-cols-2 grid-rows-2 gap-2">
         <div className="row-start-1 col-start-1">
           <ProjectOpener />
         </div>
