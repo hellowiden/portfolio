b1bb8c11558799aced56a63f7b6e61b5afa1c126
Marcus Widen <hellowiden@gmail.com>
2025-03-24 17:37:11 +0100

    theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={spaceGrotesk.variable}>
-      <body className="grid min-h-screen grid-rows-[auto_1fr_auto] bg-zinc-50 dark:bg-zinc-800 text-zinc-900 dark:text-zinc-50">
+      <body className="grid min-h-screen grid-rows-[auto_1fr_auto] bg-zinc-100 dark:bg-zinc-900 text-zinc-900 dark:text-zinc-50">
         <ThemeContextProvider>
           <AuthProvider>
             <Header />
