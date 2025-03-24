4b2b11a1736e6fe60f3c98603837f0f1a1e290a3
Marcus Widen <hellowiden@gmail.com>
2025-03-24 19:20:08 +0100

    introductionsection

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
   const isAuthenticated = status === 'authenticated';
 
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-[auto_1fr] items-center py-2 px-8 border-b border-zinc-300 dark:border-zinc-700 backdrop-blur-md bg-zinc-50/70 dark:bg-zinc-800/70 text-zinc-900 dark:text-zinc-50">
+    <header className="sticky top-0 z-50 grid grid-cols-[auto_1fr] items-center py-2 px-8 border-b border-zinc-300 dark:border-zinc-700 backdrop-blur-md bg-zinc-50/90 dark:bg-zinc-800/90 text-zinc-900 dark:text-zinc-50">
       <div className="hover:opacity-80 transition">
         <button onClick={() => router.push('/')} className="focus:outline-none">
           {isAuthenticated ? `Welcome, ${session.user?.name}` : 'MW-Portfolio'}
