2427b14cbfb9fdb2bef198a425de2e0e36f8b90b
Marcus Widen <hellowiden@gmail.com>
2025-03-26 15:26:29 +0100

    introductionsection

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -6,30 +6,30 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="w-full border-t border-zinc-300 dark:border-zinc-700 backdrop-blur-md bg-zinc-50/70 dark:bg-zinc-800/70 text-zinc-900 dark:text-zinc-50 transition">
+    <footer className="w-full border-t border-[#E3E3E3] dark:border-[#292929] backdrop-blur-md bg-[#FFFFFF]/70 dark:bg-[#191919]/70 text-[#121212] dark:text-[#FFFFFF] transition">
       <div className="w-full px-6 sm:px-8 py-10 grid gap-6 md:grid-cols-[1fr_auto]">
         <section>
-          <h4 className="text-xl font-semibold text-zinc-900 dark:text-zinc-50 mb-4">
+          <h4 className="text-xl font-semibold text-[#121212] dark:text-[#FFFFFF] mb-4">
             Portfolio Techstack
           </h4>
-          <p className="text-sm text-zinc-700 dark:text-zinc-300">
+          <p className="text-sm text-[#121212] dark:text-[#E3E3E3]">
             Proudly built with Next.js TypeScript, Tailwind CSS and Framer ·
             Made by Marcus Widén · Hosted on Vercel
           </p>
         </section>
         <nav className="md:text-right">
-          <h4 className="text-xl font-semibold text-zinc-900 dark:text-zinc-50 mb-4">
+          <h4 className="text-xl font-semibold text-[#121212] dark:text-[#FFFFFF] mb-4">
             Docs
           </h4>
           <Link
             href="/legal"
-            className="text-sm font-medium text-black/60 dark:text-green-400 hover:text-green-500 dark:hover:text-green-300 transition"
+            className="text-sm font-medium text-[#121212]/60 dark:text-[#FFFFFF] hover:text-[#121212] dark:hover:text-[#E3E3E3] transition"
           >
             Legal
           </Link>
         </nav>
       </div>
-      <div className="grid gap-4 border-t border-zinc-200 dark:border-zinc-800 p-4 text-sm text-zinc-700 dark:text-zinc-300 sm:px-8 md:grid-cols-[1fr_auto]">
+      <div className="grid gap-4 border-t border-[#F1F1F1] dark:border-[#292929] p-4 text-sm text-[#121212] dark:text-[#E3E3E3] sm:px-8 md:grid-cols-[1fr_auto]">
         <p>
           &copy; {currentYear} Marcus Widén — Personal Portfolio. All rights
           reserved.
