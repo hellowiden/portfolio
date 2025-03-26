de604c24a74a765c0ce1b88f771389ad151c5f36
Marcus Widen <hellowiden@gmail.com>
2025-03-26 17:55:51 +0100

    theme

--- src/app/components/NavLinks/NavLinks.tsx
+++ src/app/components/NavLinks/NavLinks.tsx
@@ -18,7 +18,7 @@ export default function NavLinks() {
   const [hoveredButton, setHoveredButton] = useState<string | null>(null);
 
   const buttonBaseClasses =
-    'grid grid-cols-[auto_1fr] items-center p-2 text-sm sm:gap-2 border rounded transition bg-primary-100 text-primary-900 hover:bg-primary-200 border-primary-200 dark:bg-secondary-700 dark:text-secondary-50 dark:hover:bg-secondary-800 dark:border-secondary-800';
+    'grid grid-cols-[auto_1fr] items-center p-2 text-sm sm:gap-2 border rounded transition bg-primary-100 text-primary-900 hover:bg-primary-200 border-primary-200 dark:bg-secondary-700 dark:hover:bg-secondary-800 dark:text-secondary-50';
 
   return (
     <nav className="flex justify-end items-center">
