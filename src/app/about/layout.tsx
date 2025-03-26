2de02fdd4f8d2fed48993dea07c93684fb266d6a
Marcus Widen <hellowiden@gmail.com>
2025-03-26 20:40:12 +0100

    theme

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -6,7 +6,7 @@ import { ReactNode } from 'react';
 
 export default function AboutLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="h-full flex flex-col items-center container mx-auto border-x border-primary-200 dark:border-secondary-800 backdrop-blur-md bg-primary-100/80 dark:bg-secondary-900/80">
+    <div className="h-full flex flex-col items-center container mx-auto border-x backdrop-blur-md bg-[#F1F1F1] dark:bg-[#121212] text-[#121212] dark:text-[#FFFFFF]">
       <main className="container mx-auto flex-grow">{children}</main>
     </div>
   );
