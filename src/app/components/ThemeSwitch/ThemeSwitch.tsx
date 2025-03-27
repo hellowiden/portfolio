4dcd824af3e9c6b0b3bfabb6d404579f36437730
Marcus Widen <hellowiden@gmail.com>
2025-03-27 07:53:43 +0100

    theme

--- src/app/components/ThemeSwitch/ThemeSwitch.tsx
+++ src/app/components/ThemeSwitch/ThemeSwitch.tsx
@@ -8,9 +8,9 @@ import { useTheme } from '@/context/theme-context';
 import { FiMoon, FiSun } from 'react-icons/fi';
 
 const buttonBaseClasses =
-  'grid grid-cols-[auto_1fr] items-center p-2 text-sm sm:gap-2 border rounded transition \
-  text-primary-900 bg-primary-100 hover:bg-primary-200 border-primary-200 \
-  dark:text-secondary-50 dark:bg-secondary-800 dark:hover:bg-secondary-700 dark:border-secondary-800';
+  'grid grid-cols-[auto_1fr] items-center p-2 text-sm sm:gap-2 rounded transition \
+  text-primary-900 bg-primary-100 hover:bg-primary-200  \
+  dark:text-secondary-50 dark:bg-secondary-800 dark:hover:bg-secondary-700 ';
 
 export default function ThemeSwitch() {
   const { theme, toggleTheme } = useTheme();
