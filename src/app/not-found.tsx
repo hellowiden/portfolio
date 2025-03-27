2bb631c3fcf4b22cfc8ee4ba11e1e45ab4ddf1a7
Marcus Widen <hellowiden@gmail.com>
2025-03-27 12:48:42 +0100

    theme

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,18 +3,18 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div className="grid place-items-center container mx-auto bg-zinc-100 dark:bg-zinc-800 border-x border-zinc-300 dark:border-zinc-700 min-h-full p-6">
+    <div className="grid place-items-center container mx-auto bg-primary-100 dark:bg-secondary-800 border-x border-primary-200 dark:border-secondary-700 min-h-full p-6">
       <div className="grid gap-6 text-center">
-        <h1 className="text-4xl font-bold text-zinc-900 dark:text-zinc-50">
+        <h1 className="text-4xl font-bold text-primary-900 dark:text-secondary-50">
           404 – Woopsie daisies! You’ve hit a dead end.
         </h1>
-        <p className="text-lg text-zinc-600 dark:text-zinc-400">
+        <p className="text-lg text-primary-200 dark:text-secondary-200">
           Either the file was deleted, or someone forgot to push to{' '}
           <code>main</code>. Happens to the best of us. 😘
         </p>
         <Link
           href="/"
-          className="text-base font-medium text-green-600 hover:text-green-500 dark:text-green-400 dark:hover:text-green-300 transition"
+          className="text-base font-medium text-primary-900 hover:text-primary-200 dark:text-secondary-50 dark:hover:text-secondary-200 transition"
         >
           Return to safety
         </Link>
