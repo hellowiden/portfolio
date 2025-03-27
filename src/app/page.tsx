aefae166c61ad8b88fc0c01e80e83ae6c529eccb
Marcus Widen <hellowiden@gmail.com>
2025-03-27 08:55:01 +0100

    theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,11 @@ import ClientsCarousel from '@/app/components/ClientsCarousel/ClientsCarousel';
 import ProjectOpener from '@/app/components/ProjectOpener/ProjectOpener';
 import ExperienceOpener from '@/app/components/ExperienceOpener/ExperienceOpener';
 import ContactOpener from '@/app/components/ContactOpener/ContactOpener';
-import { InteractiveGridBackground } from './components/InteractiveGridBackground/InteractiveGridBackground';
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen w-full">
-      {/* Background Layer */}
-      <div className="z-0">
-        <InteractiveGridBackground />
-      </div>
-
-      {/* Foreground Content */}
-      <div className="z-10 relative grid grid-rows-[min-content_min-content_min-content] gap-6 h-full container mx-auto p-6 border-x border-primary-200 dark:border-secondary-700 bg-primary-50 dark:bg-secondary-900 text-secondary-900 dark:text-primary-50">
+    <>
+      <div className="grid grid-rows-[min-content_min-content_min-content] gap-6 h-full container mx-auto p-6 border-x border-[#E3E3E3] dark:border-[#292929] bg-[#FFFFFF] dark:bg-[#121212] text-[#121212] dark:text-[#FFFFFF]">
         <IntroductionSection />
         <ClientsCarousel />
 
@@ -29,6 +22,6 @@ export default function Home() {
           <ContactOpener />
         </div>
       </div>
-    </div>
+    </>
   );
 }
