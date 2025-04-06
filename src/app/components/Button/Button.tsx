949aae72da15ebd527c26f2e5fb8ee66aca3a409
Marcus Widen <hellowiden@gmail.com>
2025-04-06 12:36:24 +0200

    centerlized btn

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -23,8 +23,8 @@ const variantMap = {
 
 const sizeMap = {
   sm: 'text-sm px-3 py-1.5',
-  md: 'text-sm px-4 py-2',
-  lg: 'text-base px-6 py-3',
+  md: 'text-md px-4 py-2',
+  lg: 'text-lg px-6 py-3',
 };
 
 export default function Button({
