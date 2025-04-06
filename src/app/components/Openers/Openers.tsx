450fc8f913338bf4cdb4c8328da0ac0618c860ac
Marcus Widen <hellowiden@gmail.com>
2025-04-06 13:09:30 +0200

    centerlized btn

--- src/app/components/Openers/Openers.tsx
+++ src/app/components/Openers/Openers.tsx
@@ -3,32 +3,30 @@
 import { FiMail, FiBriefcase, FiFolder } from 'react-icons/fi';
 import HoverCard from '../HoverCard/HoverCard';
 
-const openerData = {
-  row1: [
-    {
-      title: 'My Projects',
-      subtitle: 'Case Studies',
-      description:
-        'All future projects will be presented here, offering insight into ongoing work, upcoming ideas, and creative direction.',
-      imageSrc: '/projectsopener.jpg',
-      icon: <FiFolder className="text-lg" />,
-      buttonLabel: 'View Projects',
-      buttonRoute: '/projects',
-      buttonAriaLabel: 'View Projects',
-    },
-    {
-      title: 'My Experiences',
-      subtitle: 'Professional Journey',
-      description:
-        'Explore my professional journey and discover how each experience has contributed to the skills, insights, and expertise I bring today.',
-      imageSrc: '/shake.jpg',
-      icon: <FiBriefcase className="text-lg" />,
-      buttonLabel: 'View Experiences',
-      buttonRoute: '/experiences',
-      buttonAriaLabel: 'View Experiences',
-    },
-  ],
-  row2: {
+const cardData = [
+  {
+    title: 'My Projects',
+    subtitle: 'Case Studies',
+    description:
+      'All future projects will be presented here, offering insight into ongoing work, upcoming ideas, and creative direction.',
+    imageSrc: '/projectsopener.jpg',
+    icon: <FiFolder className="text-lg" />,
+    buttonLabel: 'View Projects',
+    buttonRoute: '/projects',
+    buttonAriaLabel: 'View Projects',
+  },
+  {
+    title: 'My Experiences',
+    subtitle: 'Professional Journey',
+    description:
+      'Explore my professional journey and discover how each experience has contributed to the skills, insights, and expertise I bring today.',
+    imageSrc: '/shake.jpg',
+    icon: <FiBriefcase className="text-lg" />,
+    buttonLabel: 'View Experiences',
+    buttonRoute: '/experiences',
+    buttonAriaLabel: 'View Experiences',
+  },
+  {
     title: 'Get in Touch',
     subtitle: "Let's Connect",
     description:
@@ -39,18 +37,18 @@ const openerData = {
     buttonRoute: '/contact',
     buttonAriaLabel: 'Contact Me',
   },
-};
+];
 
 export default function Openers() {
   return (
     <>
       <div className="grid gap-6 grid-cols-1 sm:grid-cols-2">
-        {openerData.row1.map((card, index) => (
-          <HoverCard key={index} {...card} heightClass="h-64" />
+        {cardData.slice(0, 2).map((card, i) => (
+          <HoverCard key={i} {...card} heightClass="h-64" />
         ))}
       </div>
       <div>
-        <HoverCard {...openerData.row2} heightClass="h-64" />
+        <HoverCard {...cardData[2]} heightClass="h-64" />
       </div>
     </>
   );
