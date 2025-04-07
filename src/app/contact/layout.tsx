06404e7a36ec79930574b49cb76899eddfa878bd
Marcus Widen <hellowiden@gmail.com>
2025-04-07 14:20:21 +0200

    centerlized btn

--- src/app/contact/layout.tsx
+++ src/app/contact/layout.tsx
@@ -6,7 +6,7 @@ import { ReactNode } from 'react';
 
 export default function ContactLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="h-full container mx-auto grid grid-rows-[auto_1fr] border-x border-primary-200 dark:border-secondary-700 backdrop-blur-md bg-primary-100/80 dark:bg-secondary-900/80">
+    <div className="h-full container mx-auto grid grid-rows-[auto_1fr] border-x border-primary-200 dark:border-secondary-700 backdrop-blur-md bg-primary-50 text-primary-900 dark:bg-secondary-900 dark:text-secondary-50">
       <header className="grid gap-0 w-full bg-primary-200 text-primary-900 dark:bg-secondary-800 dark:text-secondary-50 p-4">
         <h1 className="text-2xl font-bold">Contact Me</h1>
       </header>
