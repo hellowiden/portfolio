494e7c600fd9fb035e37a8fd5053e95a61093302
Marcus Widen <hellowiden@gmail.com>
2025-04-08 07:27:13 +0200

    centerlized btn

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -35,7 +35,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-[auto_1fr] h-full bg-primary-100 text-primary-900 dark:bg-secondary-900 dark:text-secondary-50 gap-4">
       {/* SIDEBAR NAVIGATION */}
-      <aside className="grid grid-rows-[auto_auto_1fr] gap-4 w-64 bg-primary-200 border-r border-primary-200 dark:bg-secondary-800 dark:border-secondary-700 p-4">
+      <aside className="grid grid-rows-[auto_auto_1fr] gap-4 w-full md:w-64 bg-primary-200 border-r border-primary-200 dark:bg-secondary-800 dark:border-secondary-700 p-4">
         <h2 className="text-xl font-bold">Admin Dashboard</h2>
         <nav className="grid gap-2">
           <NavItem href="/dashboard" label="Dashboard" />
