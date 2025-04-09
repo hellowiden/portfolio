867760160a975053e25437c0e7d987584676a04f
Marcus Widen <hellowiden@gmail.com>
2025-04-09 21:05:54 +0200

    onlinestatus

--- src/middleware.ts
+++ src/middleware.ts
@@ -29,3 +29,41 @@ export async function middleware(req: NextRequest) {
 
   return NextResponse.next();
 }
+
+/*
+
+// src/middleware.ts
+
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+
+export const config = {
+  matcher: [
+    '/',
+    '/dashboard/:path*',
+    '/about',
+    '/profile',
+    '/experiences/:path*',
+    '/login',
+    '/register',
+  ],
+};
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const { pathname } = req.nextUrl;
+
+  if (token && (pathname === '/login' || pathname === '/register')) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  if (!token && !['/login', '/register'].includes(pathname)) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  return NextResponse.next();
+}
+
+
+
+*/
