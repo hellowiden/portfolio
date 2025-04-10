036001f5bae69e49b52b0dfd846dcf538164fede
Marcus Widen <hellowiden@gmail.com>
2025-04-10 20:13:08 +0200

    onlinestatus

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['images.pexels.com'],
+  },
 };
 
 export default nextConfig;
