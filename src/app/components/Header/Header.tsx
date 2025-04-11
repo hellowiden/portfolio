cca5c3f6a860afde9a8cf1c3cec904c63387e83c
Marcus Widen <hellowiden@gmail.com>
2025-04-11 22:02:54 +0200

    mdb

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -11,13 +11,17 @@ export default function Header() {
   const { data: session, status } = useSession();
 
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-[auto_1fr] items-center py-2 px-8 border-b border-primary-200 dark:border-secondary-700 text-lg bg-primary-50 dark:bg-secondary-800 text-primary-900 dark:text-secondary-50">
+    <header
+      role="header"
+      aria-label="Main header"
+      className="sticky top-0 z-50 grid grid-cols-[auto_1fr] items-center py-2 px-8 border-b border-primary-200 dark:border-secondary-700 text-lg bg-primary-50 dark:bg-secondary-800 text-primary-900 dark:text-secondary-50"
+    >
       <div className="hover:opacity-80 transition">
-        <Link href="/">
+        <Link href="/" aria-label="Homepage">
           <Button
             variant="ghost"
             size="sm"
-            className="p-0 h-auto focus:outline-none font-bold"
+            className="p-0 h-auto focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-500 font-bold"
           >
             {status === 'authenticated'
               ? `Welcome, ${session.user?.name}`
