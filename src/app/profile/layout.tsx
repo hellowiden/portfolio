3e69b94b0410b49a2bc0a1067ca533e7c8e92b3f
Marcus Widen <hellowiden@gmail.com>
2025-04-11 07:08:43 +0200

    profilepage

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -6,7 +6,7 @@ import { ReactNode } from 'react';
 
 export default function ProfileLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="h-screen w-full max-w-4xl mx-auto border-x border-primary-200 dark:border-secondary-700 backdrop-blur-md bg-primary-50 dark:bg-secondary-900 text-primary-900 dark:text-secondary-50 flex flex-col">
+    <div className="h-full container mx-auto border-x border-primary-200 dark:border-secondary-700 backdrop-blur-md bg-primary-50 dark:bg-secondary-900 text-primary-900 dark:text-secondary-50 flex flex-col">
       <header className="px-4 py-3 sm:px-6 border-b border-primary-200 dark:border-secondary-700 bg-primary-200 dark:bg-secondary-800">
         <h1 className="text-xl sm:text-2xl font-bold">Profile Page</h1>
       </header>
